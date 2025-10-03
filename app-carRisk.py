1e45dc8a3f8c14c26ed81743d74448b37d3c855d
Ana Isabel Oviedo Carrascal <ana.oviedo@upb.edu.co>
2025-10-03 16:46:35 -0500

    app-carRisk.py

--- app-carRisk.py
+++ app-carRisk.py
@@ -27,8 +27,8 @@ modelTree,model_rf,modelKnn,modelNN,modelSVM, labelencoder,variables,min_max_sca
 #modelTree,modelKnn,modelNN, labelencoder,variables,min_max_scaler = pickle.load(open(filename, 'rb'))
 
 #Cargamos los datos futuros
-data = pd.read_csv("carRisk-despliegue.csv")
-data.head()
+#data = pd.read_csv("carRisk-despliegue.csv")
+#data.head()
 
 #Se crea interfaz gráfica con streamlit para captura de los datos
 
